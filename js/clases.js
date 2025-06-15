e3af81b40bd61e621d5f1021704d13f2260fd2ca
ottisosa <ottisosa10@gmail.com>
2025-06-14 23:31:07 -0300

    funcion Agregar carreras terminado

--- js/clases.js
+++ js/clases.js
@@ -1,6 +1,7 @@
-class Sistema {
+/*
+  Autores: Octavio Sosa (363131) - Alan Klein(358518)
+*/
 
-}
 
 class Carrera {
 
@@ -25,14 +26,27 @@ class Inscripcion {
 
 class Patrocinador {
 
-    //     constructor(nom, rubro, carrera) {
-    //     this.nombre = nom;
-    //     this.departamento = depto;
-    //     this.fecha = fecha;
-    //     this.cupo = cupo;
-    // }
-    // toString() {
-    //     return this.nombre + " " + this.departamento + " (" + this.fecha + ")" + this.cupo + "";
-    // }
     
+}
+
+class Sistema {
+    constructor(){
+        this.carreras = [];
+        this.corredores = [];
+        this.inscripciones = [];
+        this.patrocinadores = []; 
+    }
+        agregarCarrera(carrera){
+        this.carreras.push(carrera);
+    }
+
+     carreraExiste(carr){
+        let existe = false;
+        for (let i=0; i<this.carreras.length && !existe; i++){
+            if (this.carreras[i].nombre == carr){
+                existe = true;
+            }
+        }
+        return existe;
+    }
 }
--- js/funciones.js
+++ js/funciones.js
@@ -1,2 +1,71 @@
+/*
+  Autores: Octavio Sosa (363131) - Alan Klein(358518)
+*/
+
 window.addEventListener("load", inicio);
 
+let sistema = new Sistema();
+
+function inicio() {
+
+    document.getElementById("datos").addEventListener("click", mostrar);
+    document.getElementById("estadisticas").addEventListener("click", mostrar);
+
+
+    document.getElementById("sectionEstadisticas").style.display = "none";
+    document.getElementById("sectionDatos").style.display = "block";
+
+    document.getElementById("btnAgregarC").addEventListener("click", agregarCarrera);
+}
+
+function mostrar(e) {
+
+    if (e.target.id === "datos") {
+
+        document.getElementById("sectionEstadisticas").style.display = "none";
+        document.getElementById("sectionDatos").style.display = "block";
+
+        document.getElementById("estadisticas").classList.remove("botonSeleccionado");
+        document.getElementById("estadisticas").classList.add("ver");
+
+
+        document.getElementById("datos").classList.remove("ver");
+        document.getElementById("datos").classList.add("botonSeleccionado");
+
+    } else {
+
+        document.getElementById("sectionDatos").style.display = "none";
+        document.getElementById("sectionEstadisticas").style.display = "block";
+
+        document.getElementById("datos").classList.remove("botonSeleccionado");
+        document.getElementById("datos").classList.add("ver");
+
+
+        document.getElementById("estadisticas").classList.remove("ver");
+        document.getElementById("estadisticas").classList.add("botonSeleccionado");
+
+    }
+
+}
+
+function agregarCarrera() {
+
+    if (document.getElementById("formCarreras").reportValidity()) {
+
+        let nombre = document.getElementById("nomTxt").value;
+        let depto = document.getElementById("selectDep").value;
+        let fecha = document.getElementById("FechaCarr").value;
+        let cupo = parseInt(document.getElementById("CupoCarr").value);
+
+        if (sistema.carreraExiste(nombre)) {
+            alert("Esta carrera ya existe, pongale otro nombre");
+        }
+        else {
+            let carr = new Carrera(nombre, depto, fecha, cupo);
+            sistema.agregarCarrera(carr);
+            document.getElementById("formCarreras").reset();
+
+        }
+    }
+
+}
